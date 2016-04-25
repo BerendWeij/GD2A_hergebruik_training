8b3da5a976e53003551dbc77831a9513a137b030
ezowolf <wiggler@live.nl>
2016-04-25 12:37:38 +0200

    Hergebruik project

---
+++ Hergebruik/Assets/CameraCutscene.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCutscene : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject animateObject;
+
+	[SerializeField]
+	private CameraFollower cameraFollower;
+
+	[SerializeField]
+	private CameraShake cameraShake;
+
+	[SerializeField]
+	private GameObject animateObject2;
+
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			StartCoroutine(Cutscene());
+		}
+	}
+
+	IEnumerator Cutscene()
+	{
+		cameraFollower.AnimateTo (animateObject);
+		yield return new WaitForSeconds (2);
+		cameraShake.StartShake ();
+		yield return new WaitForSeconds (2);
+		cameraShake.StartShake ();
+		yield return new WaitForSeconds (2);
+		cameraFollower.AnimateTo (animateObject2);
+
+	}
+}
---
+++ Hergebruik/Assets/CameraFollower.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject objectToFollow;
+
+	[SerializeField]
+	private GameObject animateObject;
+
+	[SerializeField]
+	private Vector3 offset;
+
+	[SerializeField]
+	private float positionSmoothTime;
+	
+	void Update () 
+	{
+		Vector3 velocity = Vector3.zero;
+		Vector3 cameraDestination = new Vector3 (objectToFollow.transform.position.x+offset.x, objectToFollow.transform.position.y+ offset.y, objectToFollow.transform.position.z + offset.z);
+		this.transform.position = Vector3.SmoothDamp (transform.position, cameraDestination, ref velocity, positionSmoothTime);
+	}
+
+	public void AnimateTo(GameObject _objectToAnimateTo)
+	{
+		objectToFollow = _objectToAnimateTo;
+
+	}
+}
