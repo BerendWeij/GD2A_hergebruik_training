42b039a611878cd0107ae317a19d9cca5dbe6cd0
SkaterDanny <dannykruiswijk@hotmail.com>
2016-04-25 12:39:41 +0200

    First (and probably final) Commit

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+    private float counter = 1;
+    private Vector3 originalPosition;
+
+    void Start () {
+        originalPosition = transform.position;
+	}
+	
+	void Update () {
+        counter -= Time.deltaTime;
+        if (counter > 0)
+        {
+            MakeShake();
+        }
+        else {
+            counter = 0;
+            transform.position = originalPosition;
+        }
+	}
+
+    public void MakeShake()
+    {
+        float randomXValue = Random.Range(-5, 5);
+        float randomYValue = Random.Range(-5, 5);
+        transform.position = new Vector3(transform.position.x+(randomXValue/100),transform.position.y+(randomYValue/100),transform.position.z);
+    }
+}
