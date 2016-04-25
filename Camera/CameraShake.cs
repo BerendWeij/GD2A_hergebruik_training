b3555650d21495923bc62fcb667a1cca78a63244
18839 <18839@ma-web.nl>
2016-04-25 12:42:09 +0200

    Camera Scripts 25-4-2016

---
+++ Camera/CameraShake.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+    //Floats
+    [SerializeField]
+    private float _shakePower = 0.05f;
+    //Floats
+
+    //Bool
+    private bool _isShaking;
+	//Bool
+
+
+	void Update () 
+    {
+        ShakeCamera();
+	}
+
+    private void ShakeCamera()
+    {
+        if (_isShaking)
+        {
+            transform.localPosition = transform.localPosition + Random.insideUnitSphere * _shakePower;
+        }
+    }
+
+
+    public void Shake(float duration)
+    {
+        _isShaking = true;
+        CancelInvoke();
+        Invoke("StopShaking", duration);
+    }
+
+  
+
+
+    public void StopShaking()
+    {
+        _isShaking = false;
+    }
+}
