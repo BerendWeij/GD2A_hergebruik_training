aba2bdef3f64fdbf4cedd1fbf155427838ad1067
Pim Horeman <pimhoreman@gmail.com>
2016-04-25 12:40:49 +0200

    ZoomCamera
    Not Ready to use yet.

---
+++ Assets/Script/CameraFOV.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFOV : OverallCamera {
+
+	[SerializeField] private float zoomSpeed = 10;		
+	[SerializeField] private float targetFOV;			//Target Amount of FOV
+	[SerializeField] private float smoothSpeed = 5.0f;	
+	[SerializeField] private float minFOV = 1.0f;		
+	[SerializeField] private float maxFOV = 60.0f;
+	private float scroll;
+
+	public void Start() {
+		//Connects With The Main Camera
+		targetFOV = Camera.main.orthographicSize;
+
+	}
+
+	void Update()
+	{
+		scroll = Input.GetAxis ("Mouse ScrollWheel");
+		if (scroll != 0.0f) {
+			targetFOV -= scroll * zoomSpeed;
+			targetFOV = Mathf.Clamp (targetFOV, minFOV, maxFOV);
+		}
+
+		Camera.main.orthographicSize = Mathf.MoveTowards (Camera.main.orthographicSize, targetFOV, smoothSpeed * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Script/OverallCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverallCamera : MonoBehaviour {
+
+
+
+	void Start() {
+		
+	}
+
+	public void follow(Transform target){
+		
+	}
+
+	void Update(){
+		
+	}
+
+}
