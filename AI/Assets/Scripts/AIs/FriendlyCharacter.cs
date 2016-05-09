d369cc2d2a732b415ba8ef571dd735bc6641070d
DavidZwit <davidzwitser@gmail.com>
2016-05-09 11:05:34 +0200

    Adding animation support and custom navigator script support

--- AI/Assets/Scripts/AIs/FriendlyCharacter.cs
+++ AI/Assets/Scripts/AIs/FriendlyCharacter.cs
@@ -9,7 +9,7 @@ public class FriendlyCharacter : MonoBehaviour {
     {
         behaviourMachine = new StateMachine(gameObject, StatesEnum.wander); 
 
-        behaviourMachine.AddState(StatesEnum.wander, new Wander("Player"));
+        behaviourMachine.AddState(StatesEnum.wander, new Wander("Player", "", new Navigator()));
         behaviourMachine.AddState(StatesEnum.alert, new Charge("Player"));
         behaviourMachine.AddState(StatesEnum.intaract, new Attack("Player"));
         behaviourMachine.AddState(StatesEnum.retreat, new Flee("Player"));
