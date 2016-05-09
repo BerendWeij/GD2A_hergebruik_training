bdf374eebe2448184e0c9cbee9f1c1ffa1f8c083
Niels <Niels97vet@hotmail.com>
2016-05-09 11:04:11 +0200

    fix branch

---
+++ Opdracht hergebruik/Assets/scripts/CameraScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private Camera camera;
+
+    public float rotationspeed = 1f;
+    public float speed = 0.1f;
+
+    private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camera.transform.LookAt(target.transform);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(target.transform.position, Vector3.down, rotationspeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, rotationspeed);
+        }
+	}
+}
---
+++ Opdracht hergebruik/Assets/scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float rotationspeed = 1.5f;
+    public float moveSpeed = 0.2f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotationspeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotationspeed);
+        }
+    }
+}
