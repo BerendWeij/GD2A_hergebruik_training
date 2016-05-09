63ecfc66869f403bb8aa4ab6bbee26815bb18a4e
yoshalangius <henkkooi11@gmail.com>
2016-05-09 11:16:07 +0200

    fixing things

---
+++ DemoProjects2/Assets/CameraDemos/FollowCameraScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCameraScript : MonoBehaviour
+{
+
+    //in order to make a follow function that is reusable i must get the position from an object, this must be done continuaslly.
+    //how is the question.
+
+    public GameObject Target;
+
+
+	void Start ()
+    {
+	    
+	}
+	
+	
+	void Update ()
+    {
+	    
+	}
+
+    void TurnOnBrain()
+    {
+        if (BrainOn == true)
+        {
+            Debug.Log("starting up");
+        }
+    }
+}
